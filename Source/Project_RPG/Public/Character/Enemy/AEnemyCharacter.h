6c1f6bb1b7738888fff4afa2ea51da8bbc2712f2
ErickGameDev <ericksan772@gmail.com>
2024-11-07 14:17:34 -0300

    Add enemy

---
+++ Source/Project_RPG/Public/Character/Enemy/AEnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ACharacterBase.h"
+#include "AEnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_RPG_API AAEnemyCharacter : public AACharacterBase
+{
+	GENERATED_BODY()
+	
+};
